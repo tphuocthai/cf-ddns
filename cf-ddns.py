6976080b2f4fa8d25f0a09343b7b7d604a8eb5b9
Trinh Phuoc Thai <tphuocthai@gmail.com>
2016-05-28 13:03:28 +0700

    Update script to use CloudFlare API V4

--- cf-ddns.py
+++ cf-ddns.py
@@ -1,95 +1,93 @@
 #!/usr/bin/python
 
-# Update a set of A records in Cloudflare with the machine current IP address
+# Update a set of A records in Cloudflare with the network public IP address
 #
-# If you don't know your domain information (like id), you should run:
+# If you don't know your domain id, you should run:
 #
-#  curl https://www.cloudflare.com/api_json.html \
-#  -d 'a=rec_load_all' \
-#  -d 'tkn=CLOUDFLARE_TOKEN' \
-#  -d 'email=MY_EMAIL' \
-#  -d 'z=DOMAIN' >> response.json;
+#  curl -X GET "https://api.cloudflare.com/client/v4/zones?name=domain.com" \
+#       -H "Content-Type: application/json" 
+#       -H "X-Auth-Email: EMAIL" 
+#       -H "X-Auth-Key: TOKEN" >> response.json;
 #
-# In the response.json file you can find all DNS records for the domain.
+# In the response.json file you will find the zone_id of given domain.
 # Make sure this script runs on a cron job or whenever you get a new IP.
 #
-# Based on Aaron Rice : cloudflare_dynamic_dns.py (https://gist.github.com/riceo/2401865)
-#
-# @author Bernardo Rittmeyer <bernardo@rittme.com>
 # @author Trinh Phuoc Thai <tphuocthai@gmail.com>
 
-import urllib
+import requests
+import subprocess
 import json
 import time
 
 IP_FILE_NAME = '/var/log/cf-ddns/wanip.log'     # Path to the file that contains the actual/old IP address
 LOG_FILE_NAME = '/var/log/cf-ddns/logfile.log'  # Path to the log file
-TOKEN = "YOUR_API_KEY" # Your cloudflare token
+
+TOKEN = "YOUR_API_KEY"                # Your cloudflare token
 EMAIL = "YOUR_EMAIL"                  # Your cloudflare account email
 
-# This tuple should have a dictionary for each domain you want to keep up to date
-domains = (
-  {
-    "z"             : "example.com",  # zone_name
-    "id"            : "424754044",      # rec_id
-    "name"          : "name",            # name
-    "service_mode"  : 1                 # Status of CloudFlare Proxy, 1 = orange cloud, 0 = grey cloud.
-  }, )
+# List of record names by zone_id
+domains = {
+  'zone_id_hashed': 'comma separated names without space'
+}
 
-# External IP Address services we use to find our IP Address (if the first fail we use the next one, and so on)
-services = ("http://ip.appspot.com/", "http://my-ip.heroku.com/", "http://icanhazip.com", "http://checkip.dyndns.org", "http://curlmyip.com")
+headers = {
+  'content-type': "application/json",
+  'x-auth-email': EMAIL,
+  'x-auth-key': TOKEN,
+  'cache-control': "no-cache"
+}
 
 def log(message):
   with open(LOG_FILE_NAME, "a") as myfile:
     myfile.write("[%s] %s\n" % (time.strftime("%d/%m/%Y - %H:%M:%S"), message))
 
-def ddns_update(new_ip):
+
+def get_records(zone_id):
+  url = "https://api.cloudflare.com/client/v4/zones/" + zone_id + "/dns_records"
+  querystring = {"type":"A", "name": domains[zone_id]}
+  resp = requests.get(url, headers=headers, params=querystring)
+  return json.loads(resp.text)
+
+
+def update_dns_records(records, new_ip):
   "Send the DNS update request to Cloudflare API"
-  data = {
-    "a"             : "rec_edit",
-    "tkn"           : TOKEN,
-    "email"         : EMAIL,
-    "type"          : "A",
-    "content"       : new_ip.strip(),
-    "ttl"           : 1
-  }
 
-  for d in domains:
-    d.update(data)
-    try:
-      dns_response = json.loads(urllib.urlopen("https://www.cloudflare.com/api_json.html", urllib.urlencode(d)).read())
+  for rec in records:
+    url = "https://api.cloudflare.com/client/v4/zones/" + rec['zone_id'] + "/dns_records/" + rec['id']
+    payloads = {
+      "content": new_ip,
+      "type":"A",
+      "name": rec['name']
+    }
+    dns_response = json.loads(requests.put(url, data=json.dumps(payloads), headers=headers).text)
 
-      if dns_response[u'result'] == 'success':
-        log("%s IP updated to %s" % (d['name']+"."+d['z'], new_ip))
-      else:
-        log("Error Setting IP for %s" % d['name']+"."+d['z'])
-    except:
-      log("Error with cloudflare API")
+    if dns_response['success'] == True:
+      log("%s IP updated to %s" % (rec['name'], new_ip))
+    else:
+      log("Error Setting IP for %s" % rec['name'])
+      log('%s: %s' % (dns_response['errors'][0]['message'], json.dumps(dns_response)))
 
 
-#Main script
 
-#Find the new IP
-new_ip = ""
-for s in services:
-  try:
-    new_ip = urllib.urlopen(s).read()
-  except Exception:
-    pass
-  if new_ip:
-    break
+# Main script
+# Find the new IP
+new_ip = subprocess.check_output(["dig", "+short", "myip.opendns.com", "@resolver1.opendns.com"]).strip()
 
-#Find the old IP
-old_ip = ""
-noFile = False
-try:
-  with open(IP_FILE_NAME, 'r') as f:
-    old_ip = f.read()
-except IOError:
-   noFile = True
+# Find the old IP
+try: old_ip = open(IP_FILE_NAME, 'r').read()
+except IOError: old_ip = ""
 
 # If IP changed or no IP File, update IP File and send requests to Cloudflare API
-if new_ip != old_ip or noFile:
-  with open(IP_FILE_NAME,'w') as f:
+if new_ip != old_ip:
+  with open(IP_FILE_NAME, 'w') as f:
     f.write(new_ip)
-  ddns_update(new_ip)
+
+  # Do update for each zone
+  for key in domains:
+    # Get dns record datas
+    zone_data = get_records(key)
+    if zone_data['success'] == False:
+      log("Error getting zone data %s" % key)
+      continue
+
+    dns_response = update_dns_records(zone_data['result'], new_ip)
